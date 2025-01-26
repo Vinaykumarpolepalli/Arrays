ddbb3739023d39670474f7e2671cda1d75441414
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-26 14:43:26 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,13 @@
+package com.Arrays;
+
+import java.util.Arrays;
+
+public class Array4 {
+
+	public static void main(String[] args) {
+	int [] num = {1,4,2,5,6};
+   Arrays.sort(num);
+    System.out.println(num[1]);
+	}
+
+}
