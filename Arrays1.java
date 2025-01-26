1dab04f0ab8862547c8a97af4f8e83128176da15
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-26 18:13:03 +0530

    Add files via upload

---
+++ Arrays1.java
@@ -0,0 +1,33 @@
+package com.Arrays;
+
+public class Arrays1 {
+
+	public static void main(String[] args) {
+		int [] age = new int[5];
+        age[0]=1;
+        age[1]=2;
+        age[2]=3;
+        age[3]=4;
+        age[4]=5;
+        for(int i = 0;i<age.length;i++) {
+        System.out.println(i);
+        }
+        
+        String [] name = new String[3];
+        name[0]="Vinay";
+        name[1]="naveen";
+        name[2]="Vijay";
+        for(int a = 0;a<name.length;a++) {
+        	System.out.println(name[a]);
+        	
+        }
+        		
+	
+        
+       
+        
+
+	
+	}
+
+}
